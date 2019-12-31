3fc9f314999c5dc0d5921bd30b8d7ba50288cde0
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-12-31 01:00:57 -0400

    #4 Bealtifull background image

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 import Colors from '../../theme/Colors';
+import background from '../../assets/img/background.jpeg';
 
 export const LoginContainer = styled.div`
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: cover;
   height: 100vh;
   /* background: red; */
 
@@ -9,7 +13,17 @@ export const LoginContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 export const LoginPanel = styled.main`
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
   background: ${Colors.light};
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
   border-radius: 10px;
@@ -19,6 +33,8 @@ export const LoginPanel = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
+
+  animation: fade 3s;
 
   @media screen and (min-width: 400px) {
     width: 400px;
